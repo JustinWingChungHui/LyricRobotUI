6d5822b5e71ecd76085f9712a244c5f1c047c3cb
Tech Luminaire <info@techluminaire.co.uk>
2019-02-21 19:25:54 +0000

    Installed Vue JS

--- README.md
+++ README.md
@@ -1,2 +1,31 @@
-# LyricRobotUI
-Static Website for Lyric Robot
+# Lyric Robot UI
+
+VueJs html/js project to consume the Lyric Robot Azure function
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Run your tests
+```
+npm run test
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Robot" src="./assets/robot-with-a-gear.svg" height="100">
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  components: {
+    
+  },
+  data() {
+    return {
+      lyrics: [
+        "Gather in the moves me",
+        "Then will rise and you but i'ma need no fear puts a mirror how what you but i’m here",
+        "Save you remember i'll be cruel"
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+body {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin: 0;
+  padding: 0;
+}
+</style>
