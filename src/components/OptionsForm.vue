923cfd187bdd7feb30cb807a3ad8e53b8169f9d4
Tech Luminaire <info@techluminaire.co.uk>
2019-03-07 13:38:45 +0000

    Small fix to not lose rendering of advanced options on show/hide

--- src/components/OptionsForm.vue
+++ src/components/OptionsForm.vue
@@ -1,10 +1,7 @@
 <template>
     <div>
         <form class="pure-form" v-on:submit.prevent="onCreate(options)">
-            
-
             <fieldset>
-
                 <label for="lines"><strong>Lines: </strong></label>
 
                 <input type="number" class="padded-label" v-model="options.number_of_lyrics" :disabled="disabled" min="1" max="200">
@@ -18,9 +15,10 @@
 
             <fieldset>
                 <a href='#' v-on:click="show_advanced_options =!show_advanced_options">Advanced Options</a>
-                <div class="advanced-options" v-if="show_advanced_options">
+                <div class="advanced-options" v-show="show_advanced_options">
                     <SwitchControl label="Allow profanities" 
                         v-bind:disabled="disabled" 
+                        v-bind:initialValue="options.allow_profanities"
                         @onSwitchChanged="state => options.allow_profanities = state"/>
                 </div>
             </fieldset>
@@ -69,5 +67,11 @@ export default {
         margin: 5px;
         max-width: 6em;
         display: inline-block !important;
+    }
+
+    /**Stop stacking label on mobile device**/
+    .pure-form input:not([type]), .pure-form input[type="text"], .pure-form input[type="password"], .pure-form input[type="email"], .pure-form input[type="url"], .pure-form input[type="date"], .pure-form input[type="month"], .pure-form input[type="time"], .pure-form input[type="datetime"], .pure-form input[type="datetime-local"], .pure-form input[type="week"], .pure-form input[type="number"], .pure-form input[type="search"], .pure-form input[type="tel"], .pure-form input[type="color"], .pure-form label {
+	    margin-bottom: 0.3em;
+	    display: inline-block !important;
     }
 </style>
