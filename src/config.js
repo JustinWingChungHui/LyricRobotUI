9db0d6fc550aade5a8c0e15ca46039ec9ee87a70
Tech Luminaire <info@techluminaire.co.uk>
2019-03-16 13:40:33 +0000

    Updated endpoints to use fast one

--- src/config.js
+++ src/config.js
@@ -4,7 +4,6 @@ export const configs = {
     CreateLyricAPI: 'https://lyriccreator.azurewebsites.net/api/LyricCreatorFunction', //'http://localhost:7071/api/LyricCreatorFunction',
     CreateLyricAPICode: 'N87Je1kenLA0R3pihqLE466DUfYtfdu1MzzAnd8C3mJCfXLjqbTpvQ==',
 
-    ClassifyLyricAPI: 'https://lyricclassifierfunction20190308040158.azurewebsites.net/api/LyricClassifierFunction',
-    ClassifyLyricAPICode: 'DgFwjiZwDzjRQwWcydnwezBtzotbkMQyaLSri6xFuzgF9xBnu8ApjQ=='
+    ClassifyLyricAPI: 'https://lyricclassifierfunction20190308040158.azurewebsites.net/api/LyricClassifierFunctionAsync',
+    ClassifyLyricAPICode: 'PeUTMJvC84n0lwd7iRAyGM1DFX8/EHODoV2s5fD60QLs2Nni3u6gBQ=='
 }
-
